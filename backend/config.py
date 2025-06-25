0ae7f543db821703d2c8d75af44da584271034b0
Sayedtaqi313 <141312215+Sayedtaqi313@users.noreply.github.com>
2025-06-25 05:57:54 -0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,32 +1,37 @@
 # backend/config.py
 from pydantic_settings import BaseSettings
 from functools import lru_cache
+from pydantic import Extra
 
 class Settings(BaseSettings):
     PROJECT_NAME: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    
+
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
-    
+
     PORT: int
-    
+
     NEXT_PUBLIC_API_URL: str
     FRONTEND_DOMAIN: str
-    
+
     SMTP_HOST: str
     SMTP_PORT: int
     SMTP_USER: str
     SMTP_PASSWORD: str
     ADMIN_EMAIL: str
-    
+
+    AWS_REGION: str
+    S3_BUCKET_NAME: str
+
     class Config:
         env_file = ".env"
+        extra = Extra.allow  
 
     @property
     def database_url(self) -> str:
